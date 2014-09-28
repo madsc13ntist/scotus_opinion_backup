28566494e7a4420287f95aaf3fdbaa43fd4d2da3
MadSc13ntist <madsc13ntist@gmail.com>
2014-09-28 17:22:32 -0400

    tidying and commenting changes.

--- scotus_opinion_backup.py
+++ scotus_opinion_backup.py
@@ -20,21 +20,21 @@ __build__      = ""
 ###########################################################
 
 ### Import Modules ###
-import os			# for directory commands
-import re			# for regex matching of case names
-import time			# for generating the current 2-digit year.
-import hashlib		# part of script's version control (md5/build version)
-import optparse		# parse cmd line options and generate help doc
-import cookielib	# Makes the the scripted browser harder to identify as a script/bot
-import mechanize	# (browser emulation) https://pypi.python.org/pypi/mechanize/
+import os           # for directory commands
+import re           # for regex matching of case names
+import time         # for generating the current 2-digit year.
+import hashlib      # part of script's version control (md5/build version)
+import optparse     # parse cmd line options and generate help doc
+import cookielib    # Makes the the scripted browser harder to identify as a script/bot
+import mechanize    # (browser emulation) https://pypi.python.org/pypi/mechanize/
 
 ### Define Functions
 def createBrowser():
 	"""
 	Create a virtual browser to pilot requests. (harder to block)
 	"""
-	br = mechanize.Browser()		# Browser handle
-	cj = cookielib.LWPCookieJar()	# Cookie Jar
+	br = mechanize.Browser()        # Browser handle
+	cj = cookielib.LWPCookieJar()   # Cookie Jar
 	br.set_cookiejar(cj)
 	# Follows refresh 0 but not hangs on refresh > 0
 	br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
@@ -45,21 +45,21 @@ def scrapeUrls():
 	# current url prefix for slip opinion pdf listings
 	url_prefix = 'http://www.supremecourt.gov/opinions/slipopinions.aspx?Term='
 	
-	opinions = {}												# create  dict of opinions
-	for yy in range(options.year,int(time.strftime('%y'))):		# interate from 2003 - this year
-		yy = str(yy).zfill(2)									# make yy the 2-digit year value as a string
-		r = br.open(url_prefix+yy)								# open browser for yy year
-		resp_headers = r.info()									# parse response headers
+	opinions = {}                                            # create  dict of opinions
+	for yy in range(options.year,int(time.strftime('%y'))):  # interate from 2003 - this year
+		yy = str(yy).zfill(2)                                # make yy the 2-digit year value as a string
+		r = br.open(url_prefix+yy)                           # open browser for yy year
+		resp_headers = r.info()                              # parse response headers
 		
 		# Looking at some results in link format
 		opinion_links = [ x for x in br.links(url_regex='\d{2}-\d+.*\.pdf') ]
-		for l in opinion_links:			# parse only links to slip opinion
-			case = l.text												# the name of the case
+		for l in opinion_links:   # parse only links to slip opinion
+			case = l.text         # the name of the case
 			pdf_url = 'http://www.supremecourt.gov/opinions/'+l.url
 			if not options.regex:
-				opinions[case] = pdf_url								# add entry to opinion dict
-			elif re.findall(options.regex, case, re.I):					# if case name matches user provided regex...
-				opinions[case] = pdf_url								# add entry to opinion dict
+				opinions[case] = pdf_url                  # add entry to opinion dict
+			elif re.findall(options.regex, case, re.I):   # if case name matches user provided regex...
+				opinions[case] = pdf_url                  # add entry to opinion dict
 			#print("%s:\t%s" % (case, pdf_url))
 		if options.verbose:
 			print("Opinions for 20%s: %d" % (yy, len(opinion_links)))
@@ -67,33 +67,33 @@ def scrapeUrls():
 
 def checkForChanges(opinions):
 	changed = {}
-	for case in sorted(opinions):	# step through cases / pdf urls
+	for case in sorted(opinions):    # step through cases / pdf urls
 		try:
 			pdf_url = opinions[case]
 			if options.verbose:
 				print("Checking: '%s'" % (case))
-			r = br.open(pdf_url)										# connect to the pdf resource
-			resp_headers = r.info()										# capture response headers
-			last_mod_date = resp_headers.getheader('Last-Modified')		# parse last-modified datestamp
-			etag = resp_headers.getheader('ETag').strip('"')			# parse Etag header
+			r = br.open(pdf_url)                                        # connect to the pdf resource
+			resp_headers = r.info()                                     # capture response headers
+			last_mod_date = resp_headers.getheader('Last-Modified')     # parse last-modified datestamp
+			etag = resp_headers.getheader('ETag').strip('"')            # parse Etag header
 			
-			if not os.path.isdir(options.dir+os.sep+case+os.sep+last_mod_date):		# the case/mod directory was not found locally
+			if not os.path.isdir(options.dir+os.sep+case+os.sep+last_mod_date):   # the case/mod directory was not found locally
 				changed[case] = pdf_url
 				### THIS IS WHERE YOU ALERT/TAKE-ACTION WITH NEW/CHANGED DOCUMENTS.
 				### SEND A TWEET WITH python-twitter
 				### use pdfdiff and post the diffs in the pdfs
 				
 				if options.log_path:
-					log.write("%s,%s,%s,%s,%s\n" % (time.strftime("%Y%m%d-%H:%M:%S"), last_mod_date, case, etag, pdf_url))	# write entry to log
+					log.write("%s,%s,%s,%s,%s\n" % (time.strftime("%Y%m%d-%H:%M:%S"), last_mod_date, case, etag, pdf_url))   # write entry to log
 				if options.verbose:
 					print("Downloading NEW: %s\t%s\t%s\t%s" % (case, last_mod_date, etag, pdf_url))
 				else:
 					print("Downloading latest version of '%s'" % (case))
-				os.makedirs(options.dir+os.sep+case+os.sep+last_mod_date)					# create case/mod directory
-				br.retrieve(pdf_url, options.dir+os.sep+case+os.sep+last_mod_date+os.sep+os.path.basename(pdf_url))	# download the pdf.
+				os.makedirs(options.dir+os.sep+case+os.sep+last_mod_date)   # create case/mod directory
+				br.retrieve(pdf_url, options.dir+os.sep+case+os.sep+last_mod_date+os.sep+os.path.basename(pdf_url))    # download the pdf.
 		except Exception as e:
 			if options.log_path:
-				log.write("%s, Error: '%s',%s,%s\n" % (time.strftime("%Y%m%d-%H:%M:%S"), str(e), case, pdf_url))	# write entry to log
+				log.write("%s, Error: '%s',%s,%s\n" % (time.strftime("%Y%m%d-%H:%M:%S"), str(e), case, pdf_url))    # write entry to log
 			print("Error: '%s'\t%s\t%s" % (str(e), case, pdf_url))
 	return changed
 
@@ -177,21 +177,21 @@ if __name__ == "__main__":
 
 
 ##### Do the work
-br = createBrowser()									# Create virtual browser
-if not options.seconds:									# just check for changes once.
-	changes = checkForChanges(scrapeUrls())				# Check for changes
-	if changes:											# If there are any changes detected...
+br = createBrowser()                                    # Create virtual browser
+if not options.seconds:                                 # just check for changes once.
+	changes = checkForChanges(scrapeUrls())             # Check for changes
+	if changes:                                         # If there are any changes detected...
 		for case in sorted(changes):
-			print("Changes found in: %s" % (case))		# Display cases that changed
+			print("Changes found in: %s" % (case))      # Display cases that changed
 	if options.log_path:
 		log.close()
-else:													# Check for changes every n seconds
-	options.seconds = int(options.seconds)				# Make sure user submitted seconds is valid and convert to int.
-	while True:											# Loop forever
-		changes = checkForChanges(scrapeUrls())			# Check for changes
-		if changes:										# If there are any changes detected...
+else:                                                   # Check for changes every n seconds
+	options.seconds = int(options.seconds)              # Make sure user submitted seconds is valid and convert to int.
+	while True:                                         # Loop forever
+		changes = checkForChanges(scrapeUrls())         # Check for changes
+		if changes:                                     # If there are any changes detected...
 			for case in sorted(changes):
-				print("Changes found in: %s" % (case))	# Display cases that changed
+				print("Changes found in: %s" % (case))  # Display cases that changed
 		try:
 			print("[*] Sleeping for %d seconds..." % (options.seconds))
 			time.sleep(options.seconds)
